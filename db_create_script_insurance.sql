18194beb43577834007ed25c3d19fb3f42ee449b
Mewalsolo <mewaldawit4@gmail.com>
2023-09-05 19:06:49 -0400

    create script insurance

--- db_create_script_insurance.sql
+++ db_create_script_insurance.sql
@@ -1,10 +1,3 @@
-
-
-create table Car(
-car_id INT PRIMARY KEY ,car_make VARCHAR(50),
-car_model VARCHAR (50),car_year INT, 
-car_type_id INT,
-car_price_per_day int,
-availability_status BOOLEAN,
-foreign key (car_type_id) references Car_Type(car_type_id)
-)
+create table Insurance (
+insurance_id INT primary key , insurance_type VARCHAR(50),
+coverage_details TEXT );
