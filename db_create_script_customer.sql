1218c22213f43d10d3727e2a224f224d1704d120
Mewalsolo <mewaldawit4@gmail.com>
2023-09-05 17:25:40 -0400

    create script customer

---
+++ db_create_script_customer.sql
@@ -0,0 +1,8 @@
+create table Customer (
+customer_id int primary key,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+email VARCHAR(100),
+phone VARCHAR(20),
+date_of_birth DATE,
+address_id INT);
